4a29b0c804c4cf46470513dbc31ef567a7ebd88c
ginoliza <ginoangelolr@gmail.com>
2023-10-31 11:17:16 -0500

    modified icons on android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Color(0XFFF83FFD5),
         appBar: AppBar(
           title: Text(
             'I am rich',
